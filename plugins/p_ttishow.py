6213e95e91097a07328b5e29b51723cc1d598369
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-01-22 10:44:41 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -93,7 +93,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support Group',url="https://t.me/Movies_Zone_Support"),
+            InlineKeyboardButton('Support Group',url="https://t.me/Goldmines_Officialy"),
             InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=f"t.me/{BOT_USERNAME}")
         ],[
             InlineKeyboardButton('Use Me Here', url=f'https://t.me/{SUPPORT_CHAT}')
