6570a846ed74bab598c9d881f6dc621ed53fcdd4
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-26 13:42:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,8 +53,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 BOT_USERNAME = environ.get('BOT_USERNAME', 'Netflixbest_bot')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'links4money.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '8ec10d855e10f9743886626ba9bb5cda9c7ae3e9')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
